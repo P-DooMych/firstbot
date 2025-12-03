61f078a866806e0a4273411f295e305e9695b7e6
P-DooMych <doomych3d@gmail.com>
2025-12-04 00:03:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from openai import OpenAI
 
-load_dotenv()
+#load_dotenv()
 
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
