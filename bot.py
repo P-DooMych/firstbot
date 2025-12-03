2cf657a9c25811f5268bcac2dc9e0c261629c4dc
P-DooMych <doomych3d@gmail.com>
2025-12-03 21:22:14 +0200

    changed to webnook

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 import os
 import telebot
+from fastapi import FastAPI, Request
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, threaded=False)
+app = FastAPI()
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     bot.reply_to(message, "Hello!!")
 
-if __name__ == "__main__":
-    bot.infinity_polling(skip_pending=True)
+@app.post("/webhook")
+async def process_webhook(request: Request):
+    json_data = await request.json()
+    update = telebot.types.Update.de_json(json_data)
+    bot.process_new_updates([update])
+    return {"ok": True}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
